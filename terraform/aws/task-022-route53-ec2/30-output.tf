58ca775a268f118a944ffb88108bcaef38090b7b
codeaprendiz <codeaprendiz@gmail.com>
2023-07-29 10:47:27 +0400

    chore: update

--- terraform/aws/task-022-route53-ec2/30-output.tf
+++
@@ -1,9 +0,0 @@
-output "devopslink-public-zone-id" {
-  value = aws_route53_zone.devopslink-public-zone.zone_id
-}
-
-output "devopslink-name-servers" {
-  value = aws_route53_zone.devopslink-public-zone.name_servers
-}
-
-
--- terraform/aws/task-025-terragrunt-iam-user/05-provider.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region     = var.region
-}
-
-locals {
-  awsregion = var.region
-}
